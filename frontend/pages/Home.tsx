80d9430f0a515d46f875249fd8086894e7ba7e8d
Rishabh Kumar <97455703+rishabhxchoudhary@users.noreply.github.com>
2024-12-20 18:32:45 +0530

    commit

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,10 +1,12 @@
 // import React from 'react'
 import Layout from '../layouts/Layout'
+import { useProjectStore } from '../store/projectStore'
 
 const Home = () => {
+  const project = useProjectStore((state) => state.currentProject);
   return (
     <Layout>
-        <div>Home</div>
+        <div>{project.id} {project.name}</div>
     </Layout>
   )
 }
