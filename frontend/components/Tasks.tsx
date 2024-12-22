0087c2a7c0a253ed6878036b2b44689ca07b237b
Rishabh Kumar <97455703+rishabhxchoudhary@users.noreply.github.com>
2024-12-22 14:19:28 +0530

    separate component for tasks

---
+++ frontend/components/Tasks.tsx
@@ -0,0 +1,191 @@
+import React from "react";
+import TaskCard from "./TaskCard";
+import AddTaskCard from "./AddTask";
+import {
+  Button,
+  DateInput,
+  Divider,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Radio,
+  RadioGroup,
+  useDisclosure,
+} from "@nextui-org/react";
+import { TaskInterface } from "../types/types";
+import { useNavigate } from "react-router-dom";
+import { CalendarDate, getLocalTimeZone, today } from "@internationalized/date";
+import { createTask, getTasks } from "../src/api/task";
+import { toast } from "sonner";
+
+function getPriorityIndex(priority: string) {
+  switch (priority) {
+    case "low":
+      return 5;
+    case "medium":
+      return 4;
+    case "high":
+      return 3;
+    case "urgent":
+      return 2;
+    default:
+      return 1000;
+  }
+}
+
+const Tasks = ({ tasks, project, setTasks }) => {
+  const navigate = useNavigate();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [taskTitle, setTaskTitle] = React.useState("");
+  const [taskDescription, setTaskDescription] = React.useState("");
+  const [taskDeadlineDate, setTaskDeadlineDate] =
+    React.useState<CalendarDate | null>(null);
+  const [taskPriority, setTaskPriority] = React.useState<
+    "low" | "medium" | "high" | "urgent"
+  >("low");
+
+  const addTask = async () => {
+    if (!project) return;
+    let taskDeadlineDate2 = 0;
+    if (taskDeadlineDate) {
+      const utcDate = Date.UTC(
+        taskDeadlineDate.year,
+        taskDeadlineDate.month - 1,
+        taskDeadlineDate.day
+      );
+      taskDeadlineDate2 = Math.floor(utcDate / 1000);
+    }
+    await createTask(
+      taskTitle,
+      taskDescription,
+      project.id,
+      taskPriority,
+      taskDeadlineDate2
+    );
+    const data = await getTasks(project.id);
+    toast.success("Task Created Successfully");
+    // Sort them by priority, priority low, medium, high, urgent
+    data.sort((a, b) => {
+      return getPriorityIndex(a.priority) - getPriorityIndex(b.priority);
+    });
+    setTasks(data);
+    onOpen();
+    setTaskTitle("");
+    setTaskDescription("");
+    setTaskDeadlineDate(null);
+    setTaskPriority("low");
+  };
+  
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto p-4">
+        <div
+          className="grid gap-4"
+          style={{
+            gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+          }}
+        >
+          {tasks.map((task: TaskInterface) => (
+            <div key={task.id} onClick={() => navigate(`/project/${task.id}`)}>
+              <TaskCard
+                id={task.id}
+                title={task.title}
+                description={task.description}
+                deadlineDate={task.deadlineDate}
+                priority={task.priority}
+                board_data={task.board_data}
+              />
+            </div>
+          ))}
+          <AddTaskCard onAdd={onOpen} />
+        </div>
+      </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} closeButton>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>Create New Task</ModalHeader>
+              <ModalBody>
+                <Input
+                  label="Task Title"
+                  placeholder="Enter Task Title"
+                  value={taskTitle}
+                  onChange={(e) => setTaskTitle(e.target.value)}
+                  required={true}
+                  isRequired={true}
+                />
+                <Input
+                  label="Task Description"
+                  placeholder="Enter Task Description"
+                  value={taskDescription}
+                  onChange={(e) => setTaskDescription(e.target.value)}
+                  required={true}
+                  isRequired={true}
+                />
+                <Divider />
+                <RadioGroup
+                  color="success"
+                  size="sm"
+                  value={taskPriority}
+                  onValueChange={(value: string) => {
+                    const validPriorities = [
+                      "low",
+                      "medium",
+                      "high",
+                      "urgent",
+                    ] as const;
+                    if (
+                      validPriorities.includes(
+                        value as (typeof validPriorities)[number]
+                      )
+                    ) {
+                      setTaskPriority(
+                        value as "low" | "medium" | "high" | "urgent"
+                      );
+                    } else {
+                      console.warn(`Invalid priority value: ${value}`);
+                    }
+                  }}
+                  label="Set the priority of this task"
+                  orientation="horizontal"
+                >
+                  <Radio value="urgent">Urgent</Radio>
+                  <Radio value="high">High</Radio>
+                  <Radio value="medium">Medium</Radio>
+                  <Radio value="low">Low</Radio>
+                </RadioGroup>
+                <Divider />
+                <DateInput
+                  size="sm"
+                  value={taskDeadlineDate}
+                  onChange={(e) => setTaskDeadlineDate(e)}
+                  label="Deadline Date"
+                  minValue={today(getLocalTimeZone())}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" onPress={onClose}>
+                  Cancel
+                </Button>
+                <Button
+                  color="success"
+                  onPress={addTask}
+                  isDisabled={
+                    taskTitle.trim() === "" || taskDescription.trim() === ""
+                  }
+                >
+                  Create
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+
+export default Tasks;
--- frontend/pages/Project.tsx
+++ frontend/pages/Project.tsx
@@ -1,26 +1,10 @@
 import React, { useEffect } from "react";
 import Layout from "../layouts/Layout";
-import {
-  Button,
-  DateInput,
-  Divider,
-  Input,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  useDisclosure,
-} from "@nextui-org/react";
-import { CalendarDate, getLocalTimeZone, today } from "@internationalized/date";
-import { RadioGroup, Radio } from "@nextui-org/react";
 import { TaskInterface } from "../types/types";
-import TaskCard from "../components/TaskCard";
-import AddTaskCard from "../components/AddTask";
-import { createTask, getTasks } from "../src/api/task";
+import { getTasks } from "../src/api/task";
 import { useProjectStore } from "../store/projectStore";
 import { toast } from "sonner";
-import { useNavigate } from "react-router-dom";
+import Tasks from "../components/Tasks";
 
 function getPriorityIndex(priority: string) {
   switch (priority) {
@@ -37,9 +21,7 @@ function getPriorityIndex(priority: string) {
   }
 }
 
-
 const Project = () => {
-  const navigate = useNavigate();
   const project = useProjectStore((state) => state.currentProject);
   const [tasks, setTasks] = React.useState<TaskInterface[]>([]);
   useEffect(()=>{
@@ -55,154 +37,13 @@ const Project = () => {
     getData();
   },[project?.id])
 
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   
-  const addTask = async () => {
-    if (!project) return;
-    let taskDeadlineDate2 = 0;
-    if (taskDeadlineDate) {
-      const utcDate = Date.UTC(
-        taskDeadlineDate.year,
-        taskDeadlineDate.month - 1,
-        taskDeadlineDate.day
-      );
-      taskDeadlineDate2 = Math.floor(utcDate / 1000);
-    }
-    await createTask(
-      taskTitle,
-      taskDescription,
-      project.id,
-      taskPriority,
-      taskDeadlineDate2
-    );
-    const data = await getTasks(project.id);
-    toast.success("Task Created Successfully");
-    // Sort them by priority, priority low, medium, high, urgent
-    data.sort((a, b) => {
-      return getPriorityIndex(a.priority) - getPriorityIndex(b.priority);
-    });
-    setTasks(data);
-    onOpen();
-    setTaskTitle("");
-    setTaskDescription("");
-    setTaskDeadlineDate(null);
-    setTaskPriority("low");
-  };
   
-  const [taskTitle, setTaskTitle] = React.useState("");
-  const [taskDescription, setTaskDescription] = React.useState("");
-  const [taskDeadlineDate, setTaskDeadlineDate] =
-    React.useState<CalendarDate | null>(null);
-  const [taskPriority, setTaskPriority] = React.useState<
-    "low" | "medium" | "high" | "urgent"
-  >("low");
+  
+  
   return (
     <Layout>
-      <div className="max-w-7xl mx-auto p-4">
-        Project Id: {project?.id}
-        Project Name: {project?.name} 
-        <div
-          className="grid gap-4"
-          style={{
-            gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
-          }}
-        >
-          {tasks.map((task) => (
-            <div key={task.id} onClick={() => navigate(`/project/${task.id}`)}>
-            <TaskCard
-              id={task.id}
-              title={task.title}
-              description={task.description}
-              deadlineDate={task.deadlineDate}
-              priority={task.priority}
-              board_data={task.board_data}
-            />
-            </div>
-          ))}
-          <AddTaskCard onAdd={onOpen} />
-        </div>
-      </div>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} closeButton>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader>Create New Task</ModalHeader>
-              <ModalBody>
-                <Input
-                  label="Task Title"
-                  placeholder="Enter Task Title"
-                  value={taskTitle}
-                  onChange={(e) => setTaskTitle(e.target.value)}
-                  required={true}
-                  isRequired={true}
-                />
-                <Input
-                  label="Task Description"
-                  placeholder="Enter Task Description"
-                  value={taskDescription}
-                  onChange={(e) => setTaskDescription(e.target.value)}
-                  required={true}
-                  isRequired={true}
-                />
-                <Divider />
-                <RadioGroup
-                  color="success"
-                  size="sm"
-                  value={taskPriority}
-                  onValueChange={(value: string) => {
-                    const validPriorities = [
-                      "low",
-                      "medium",
-                      "high",
-                      "urgent",
-                    ] as const;
-                    if (
-                      validPriorities.includes(
-                        value as (typeof validPriorities)[number]
-                      )
-                    ) {
-                      setTaskPriority(
-                        value as "low" | "medium" | "high" | "urgent"
-                      );
-                    } else {
-                      console.warn(`Invalid priority value: ${value}`);
-                    }
-                  }}
-                  label="Set the priority of this task"
-                  orientation="horizontal"
-                >
-                  <Radio value="urgent">Urgent</Radio>
-                  <Radio value="high">High</Radio>
-                  <Radio value="medium">Medium</Radio>
-                  <Radio value="low">Low</Radio>
-                </RadioGroup>
-                <Divider />
-                <DateInput
-                  size="sm"
-                  value={taskDeadlineDate}
-                  onChange={(e) => setTaskDeadlineDate(e)}
-                  label="Deadline Date"
-                  minValue={today(getLocalTimeZone())}
-                />
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" onPress={onClose}>
-                  Cancel
-                </Button>
-                <Button
-                  color="success"
-                  onPress={addTask}
-                  isDisabled={
-                    taskTitle.trim() === "" || taskDescription.trim() === ""
-                  }
-                >
-                  Create
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+      <Tasks project={project} tasks={tasks} setTasks={setTasks} />
     </Layout>
   );
 };
